d7c587386f061641fa50c62be2b0068936883933
alekm <alekmalawski123@gmail.com>
2024-10-15 17:01:21 +0200

    basic UI

---
+++ lib/homepage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static int numberInRow = 11;
+  int numberOfSquares = numberInRow * 17;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: Container(
+              child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: numberOfSquares,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: numberInRow),
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(1),
+                      child: Container(
+                        color: Colors.grey,
+                        child: Text(index.toString()),
+                      ),
+                    );
+                  }),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Score: ",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  Text(
+                    "P L A Y ",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
