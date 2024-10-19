7ff1f08dbc113e0aea586d4a63960cae85eb8463
alekm <alekmalawski123@gmail.com>
2024-10-19 15:17:36 +0200

    add Vigenere encryption logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'vigenere_logic.dart';
+
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text("Hello World"),
         ),
       ),
     );
---
+++ lib/vigenere_logic.dart
@@ -0,0 +1,78 @@
+// String alphabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż";
+String alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+String filterUnencrypted(String unencrypted) {
+  String clearUnencrypted = '';
+  unencrypted = unencrypted.toLowerCase();
+  for (int i = 0; i < unencrypted.length; i++) {
+    if (alphabet.contains(unencrypted[i])) {
+      clearUnencrypted += unencrypted[i];
+    }
+  }
+  return clearUnencrypted;
+}
+
+String undeToKey(String unencrypted, String key) {
+  String unencryptedKey = '';
+  key = key.toLowerCase();
+  for (int i = 0; i < unencrypted.length; i++) {
+    unencryptedKey += key[i % key.length];
+  }
+
+  return unencryptedKey;
+}
+
+// String encrypt(String unencrypted, String key) {
+//   unencrypted = filterUnencrypted(unencrypted);
+//   String unencryptedKey = undeToKey(unencrypted, key);
+//   String encrypted = '';
+//   int row = 0;
+//   int column = 0;
+//   int charPosition = 0;
+//   for (int i = 0; i < unencrypted.length; i++) {
+//     row = alphabet.indexOf(unencrypted[i]) + 1;
+//     column = alphabet.indexOf(unencryptedKey[i]) + 1;
+//     charPosition = (row + column - 2) % alphabet.length;
+//     encrypted += alphabet[charPosition];
+//   }
+//   return encrypted;
+// }
+
+//  /|\ easier to read /|\
+
+String encrypt(String unencrypted, String key) {
+  unencrypted = filterUnencrypted(unencrypted);
+  String unencryptedKey = undeToKey(unencrypted, key);
+  String encrypted = '';
+  int charPosition = 0;
+  for (int i = 0; i < unencrypted.length; i++) {
+    charPosition =
+        (alphabet.indexOf(unencrypted[i]) + alphabet.indexOf(unencryptedKey[i])) % alphabet.length;
+    encrypted += alphabet[charPosition];
+  }
+  return encrypted;
+}
+
+String deencrypt(String encrypted, String key) {
+  String deenctypted = '';
+  String encryptedKey = undeToKey(encrypted, key);
+  int charPosition = 0;
+  for (int i = 0; i < encrypted.length; i++) {
+    charPosition += alphabet.indexOf(encrypted[i]) - alphabet.indexOf(encryptedKey[i]);
+    deenctypted += alphabet[charPosition];
+    charPosition = 0;
+    // long story short, remember to reset variable bc debuging takes a lot of time and nerves :)
+  }
+
+  return deenctypted;
+}
+
+void main() {
+  // print(undeToKey(filterUnencrypted(unencryptedDebug), keyDebug));
+  // print(unencryptedDebug.replaceAll(" ", ""));
+  String keyDebug = 'TAJNE';
+  String unencryptedDebug = 'TO JEST BARDZO TAJNY TEKST';
+
+  print(encrypt(unencryptedDebug, keyDebug));
+  print(deencrypt("mosrwmbjehsocnngycrolt", keyDebug));
+}
