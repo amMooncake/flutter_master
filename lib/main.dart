7b9c83e09d228288d66b24821e83096c6d59e658
alekm <alekmalawski123@gmail.com>
2024-10-19 17:02:20 +0200

    I made ui all by myself, and szymon criticized me

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,102 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
+  static Color top = Colors.white;
+  static Color left = Color(0xff9FC131);
+  static Color right = Color(0xff005C53);
+  static Color bottom = Color(0xffD6D58E);
+  static EdgeInsetsGeometry myPadding = EdgeInsets.all(10);
+  static BorderRadiusGeometry myRadious = BorderRadius.all(Radius.circular(30));
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
+        appBar: AppBar(
+          toolbarHeight: 150,
+          centerTitle: true,
+          backgroundColor: top,
+          title: Text("Szyfr Vigenère’a",
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(fontSize: 40)),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                          top: 10,
+                          bottom: 10,
+                        ),
+                        child: ClipRRect(
+                          borderRadius: myRadious,
+                          child: Container(
+                            height: double.infinity,
+                            color: left,
+                            child: const Padding(
+                              padding: EdgeInsets.all(20),
+                              child: Text("data"),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: myRadious,
+                                child: Container(
+                                  color: right,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                              child: ClipRRect(
+                                borderRadius: myRadious,
+                                child: Container(
+                                  color: right,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                  child: ClipRRect(
+                    borderRadius: myRadious,
+                    child: Container(
+                      color: bottom,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
