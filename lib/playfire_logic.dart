196a8105237abf37936cde8970a5e90874e90b7e
alekm <alekmalawski123@gmail.com>
2024-10-27 12:53:38 +0100

    playfire logic finished

--- lib/playfire_logic.dart
+++ lib/playfire_logic.dart
@@ -1,27 +1,27 @@
 // ignore_for_file: avoid_print
 
-// var alphabet = [
-//   ['a', 'ą', 'b', 'c', 'ć', 'd', 'e'],
-//   ['ę', 'f', 'g', 'h', 'i', 'j', 'k'],
-//   ['l', 'ł', 'm', 'n', 'ń', 'o', 'ó'],
-//   ['p', 'q', 'r', 's', 'ś', 't', 'u'],
-//   ['w', 'v', 'x', 'y', 'z', 'ź', 'ż'],
-// ];
+var alphabet = [
+  ['a', 'ą', 'b', 'c', 'ć', 'd', 'e'],
+  ['ę', 'f', 'g', 'h', 'i', 'j', 'k'],
+  ['l', 'ł', 'm', 'n', 'ń', 'o', 'ó'],
+  ['p', 'q', 'r', 's', 'ś', 't', 'u'],
+  ['w', 'v', 'x', 'y', 'z', 'ź', 'ż'],
+];
 //7 x 5
 
-var alphabet = [
-  ['a', 'b', 'C', 'D', 'E'],
-  ['F', 'G', 'H', 'I', 'J'],
-  ['K', 'L', 'M', 'N', 'O'],
-  ['P', 'Q', 'R', 'S', 'T'],
-  ['U', 'V', 'W', 'X', 'Y'],
-]; // 5 x 5
+// var alphabet = [
+//   ['a', 'b', 'c', 'd', 'e'],
+//   ['f', 'g', 'h', 'i', 'j'],
+//   ['k', 'l', 'm', 'n', 'o'],
+//   ['p', 'q', 'r', 's', 't'],
+//   ['u', 'v', 'w', 'x', 'y'],
+// ]; // 5 x 5
 
 var repetitions = ['x', 'y', 'z'];
 
 String tekstRefactor(String tekst) {
   String refactored = '';
-  tekst = tekst.replaceAll(' ', '').toLowerCase();
+  tekst = tekst.replaceAll(RegExp(r'[^a-ząćęłńóśźż]'), '');
 
   for (int i = 0; i < tekst.length - 1; i++) {
     if (tekst[i] == tekst[i + 1]) {
@@ -72,22 +72,45 @@ String playFireEncription(String tekst) {
     int col1 = getLetterColumn(char1);
     int col2 = getLetterColumn(char2);
     if (row1 == row2) {
-      tekstList[i] = '${alphabet[row1][(col1 + 1) % 7]}${alphabet[row2][(col2 + 1) % 7]}';
+      tekstList[i] =
+          '${alphabet[row1][(col1 + 1) % alphabet[0].length]}${alphabet[row2][(col2 + 1) % alphabet[0].length]}';
     } else if (col1 == col2) {
-      tekstList[i] = '${alphabet[(row1 + 1) % 5][col1]}${alphabet[(row2 + 1) % 5][col2]}';
+      tekstList[i] = '${alphabet[(row1 + 1) % alphabet.length][col1]}${alphabet[(row2 + 1) % 5][col2]}';
     } else {
       tekstList[i] = '${alphabet[row1][col2]}${alphabet[row2][col1]}';
     }
   }
-  print(tekstList);
 
   return tekstList.join(' ');
 }
 
+String playFireDecription(String tekst) {
+  var tekstList = tekst.split(' ');
+  for (int i = 0; i < tekstList.length; i++) {
+    String char1 = tekstList[i][0];
+    String char2 = tekstList[i][1];
+    int row1 = getLetterRow(char1);
+    int row2 = getLetterRow(char2);
+    int col1 = getLetterColumn(char1);
+    int col2 = getLetterColumn(char2);
+    if (row1 == row2) {
+      tekstList[i] =
+          '${alphabet[row1][(col1 - 1) % alphabet[0].length]}${alphabet[row2][(col2 - 1) % alphabet[0].length]}';
+    } else if (col1 == col2) {
+      tekstList[i] = '${alphabet[(row1 - 1) % alphabet.length][col1]}${alphabet[(row2 - 1) % 5][col2]}';
+    } else {
+      tekstList[i] = '${alphabet[row1][col2]}${alphabet[row2][col1]}';
+    }
+  }
+
+  return tekstList.join('');
+}
+
 void main() {
-  String tekst = 'wikipedia jest najlepsza';
-  print(tekstRefactor(tekst));
-  print(getLetterRow('u'));
-  print(getLetterColumn('u'));
-  print(playFireEncription(tekst));
+  String tekst = 'wiki!pedia /!@!1 jest 235naj124lepszauża';
+  print(tekst);
+  String encrypted = playFireEncription(tekst);
+  print(encrypted);
+  String decrypted = playFireDecription(encrypted);
+  print(decrypted);
 }
