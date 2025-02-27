08b93d7b9d2f69feb22d8a1c461bcf975cfad614
alekm <alekmalawski123@gmail.com>
2025-02-27 09:50:53 +0100

    .

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'blocs/authentication_bloc/authentication_bloc.dart';
 import 'screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
-import 'package:pizza_repository/pizza_repository.dart';
 
 import 'screens/auth/views/welcome_screen.dart';
 import 'screens/home/views/home_screen.dart';
--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../auth/blocs/sign_in_bloc/sign_in_bloc.dart';
@@ -7,9 +8,49 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("data"),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Row(
+          children: [
+            Image.asset('assets/8.png', scale: 14),
+            const SizedBox(width: 10),
+            const Text('PIZZA', style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30)),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.cart),
+          ),
+          IconButton(
+            onPressed: () {
+              context.read<SignInBloc>().add(SignOutRequired());
+            },
+            icon: const Icon(CupertinoIcons.square_arrow_right),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 9 / 16,
+          ),
+          itemCount: 4,
+          itemBuilder: (context, int i) {
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.red,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
